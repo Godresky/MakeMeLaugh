e782a19caf66dc24187e3b24d37bcb4980d07a9f
Sarchuk-Roman <romausb207@gmail.com>
2024-01-26 21:44:24 +0200

    Record the order on UI paper

--- Assets/Scripts/Visitor/OrderPaper.cs
+++ Assets/Scripts/Visitor/OrderPaper.cs
@@ -5,16 +5,66 @@ using UnityEngine;
 
 public class OrderPaper : MonoBehaviour, IInteractableWithPlayerObject
 {
-    [SerializeField]
+    //[SerializeField]
     private OrderPaperUI _paperUI;
     [SerializeField]
     private float _delayTime;
 
+    [Header("Wish Setting")]
+    private string[] _simpleFoodList =
+    {
+        "Egg",
+        "Milk",
+        "Yeast",
+    };
+    private string[] _hardFoodList =
+    {
+        "Flour",
+        "RoundBread",
+    };
+    [SerializeField]
+    private int _simpleFoodCount;
+    [SerializeField]
+    private int _hardFoodCount;
+
+    [Header("TESTING")]
+    [SerializeField]
+    private List<string> _wishList;
+
+    private void Start()
+    {
+        _paperUI = FindObjectOfType<OrderPaperUI>();
+
+        System.Random random = new System.Random();
+        for (int i = 0; i < _simpleFoodCount; i++)
+        {
+
+            _wishList.Add((_simpleFoodList[random.Next(0, _simpleFoodList.Length)]));
+        }
+        for (int i = 0; i < _hardFoodCount; i++)
+        {
+
+            _wishList.Add((_hardFoodList[random.Next(0, _hardFoodList.Length)]));
+        }
+    }
+
     public void Interact()
     {
         gameObject.SetActive(false);
+        _paperUI.ChangeText(GetOrderText());
         SwitchAtionUI();
         Invoke(nameof(SwitchAtionUI), _delayTime);
+    }
+
+    private string GetOrderText()
+    {
+        string text = "";
+        for (int i = 0, count = _wishList.Count; i < count; i++)
+        {
+            text += "[ ] " + _wishList[i] + "\n";
+        }
+        Debug.Log(text);
+        return text;
     }
 
     private void SwitchAtionUI()
--- Assets/Scripts/Visitor/VisiterAI.cs
+++ Assets/Scripts/Visitor/VisiterAI.cs
@@ -10,55 +10,43 @@ public class VisiterAI : MonoBehaviour
     [SerializeField]
     private GameObject _tablePoint;   // setted
     private Vector3 _startPosition;
-    [SerializeField]
     private NavMeshAgent _agent;
     [SerializeField]
     private float _waitTime;          // setted
 
     // Texture Settings
-    [Header("Texture Settings")]
-    [SerializeField]
     private GameObject _faceFuny;
-    [SerializeField]
     private GameObject _faceSad;
-    [SerializeField]
     private GameObject _faceNone;
 
     // Orders Setting
-    [Header("Orders Setting")]
-    [SerializeField]
-    private GameObject _order;
+    private OrderPaper _order;
 
     void Start()
     {
         _startPosition = GetComponent<Transform>().position;
         _agent = GetComponent<NavMeshAgent>();
-        //GameObject[] gameObj = GetComponentsInChildren<GameObject>();
-        //for (int i = 0; i < gameObject.transform.childCount; i++)
-        //{
-        //    if (gameObj[i].name == "_FunyHead")
-        //    {
-        //        Debug.Log("F");
-        //        _faceFuny = gameObj[i];
-        //    }
-        //    else if (gameObj[i].name == "_SadHead")
-        //    {
-        //        Debug.Log("S");
-        //        _faceSad = gameObj[i];
-        //    }
-        //    else if (gameObj[i].name == "_NoneHead")
-        //    {
-        //        Debug.Log("N");
-        //        _faceNone = gameObj[i];
-        //    }
-        //    else if (gameObj[i].name == "_Order")
-        //    {
-        //        Debug.Log("O");
-        //        _order = gameObj[i];
-        //    }
-        //}
+        for (int i = 0, children = transform.childCount; i < children; i++)
+        {
+            if (transform.GetChild(i).name == "_FunyHead")
+            {
+                _faceFuny = transform.GetChild(i).gameObject;
+            }
+            else if (transform.GetChild(i).name == "_SadHead")
+            {
+                _faceSad = transform.GetChild(i).gameObject;
+            }
+            else if (transform.GetChild(i).name == "_NoneHead")
+            {
+                _faceNone = transform.GetChild(i).gameObject;
+            }
+            else if (transform.GetChild(i).name == "_Order")
+            {
+                _order = transform.GetChild(i).gameObject.GetComponent<OrderPaper>();
+            }
+        }
         SetMood("none");
-        _order.SetActive(false);
+        _order.gameObject.SetActive(false);
     }
 
     public void SetMoveSetting(GameObject tablePoint, float waitTime)
